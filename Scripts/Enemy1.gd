321a0432afeb2da7216e0f4aae7303e6d894eee6
Rex <jemmyperez28@gmail.com>
2024-05-18 11:16:59 -0500

    Enemigos y bugs menores

---
+++ Scripts/Enemy1.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+
+	velocity.y += gravity * delta
+	move_and_slide()
+
+func destroy():
+	queue_free()
