af883885caee3c0415897ab5dc3d1548775c24d6
Rex <jemmyperez28@gmail.com>
2024-05-19 22:49:48 -0500

    Enemigos Random initialize

--- Scripts/Enemy1.gd
+++ Scripts/Enemy1.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var speed = 3000
 func _physics_process(delta):
 
+	velocity.x = -speed * delta
 	velocity.y += gravity * delta
 	move_and_slide()
 
---
+++ Scripts/spawn_enemy.gd
@@ -0,0 +1,25 @@
+extends Area2D
+@onready var enemy = load("res://Scenes/Enemy1.tscn")
+var spawn = true
+var random = RandomNumberGenerator.new()
+
+func _ready():
+	random.randomize()
+
+func _process(delta):
+	spawn_enemy()
+
+func spawn_enemy():
+	if spawn == true:
+		$Timer.start()
+		spawn = false
+		var EnemyInstance = enemy.instantiate()
+		EnemyInstance.position = Vector2(random.randi_range(350,400),random.randi_range(30,100))
+		add_child(EnemyInstance)
+		
+		
+
+
+func _on_timer_timeout():
+	print("spawniee")
+	spawn = true
