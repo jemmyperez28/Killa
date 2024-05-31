460d8d5ad055aec0d76f952d7268af7ab911a373
Rex <jemmyperez28@gmail.com>
2024-05-31 02:27:54 -0500

    barra de vida 1

---
+++ Scripts/health_bar_player_1.gd
@@ -0,0 +1,19 @@
+extends ProgressBar
+
+@export var player : Node
+var a : int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.hp = 99
+	player.cambio_vida.connect(update)
+	update()
+
+func update():
+	print(player.hp)
+	value = player.hp
+			
+	#value = player.hp * 100 / player.maxHealth
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	value = player.hp * 100 / player.maxHealth
+	
