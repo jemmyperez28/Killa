646edd915aa77049afe4bcaab269f742c765e74d
Rex <jemmyperez28@gmail.com>
2024-06-02 15:49:20 -0500

    Barra de Vida fix2

---
+++ Scripts/texture_progress_bar_player1.gd
@@ -0,0 +1,14 @@
+extends TextureProgressBar
+
+@export var player : Node
+var a : int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player.hp = 99
+	print(player.cambio_vida)
+	player.cambio_vida.connect(update)
+	update()
+
+func update():
+	print(player.hp)
+	value = player.hp * 100 / player.maxHealth
